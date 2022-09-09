fac251553c1a3c1d88c2d914c3eebbccdd62c84f
Jannis Harder <me@jix.one>
2022-09-09 14:17:48 +0200

    XXX tests/verious/async try aldffe

--- tests/various/async.sh
+++ tests/various/async.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-../../yosys -q -o async_syn.v -r uut -p 'synth; rename uut syn' async.v
-../../yosys -q -o async_prp.v -r uut -p 'prep; rename uut prp' async.v
-../../yosys -q -o async_a2s.v -r uut -p 'prep; async2sync -neghold; rename uut a2s' async.v
-../../yosys -q -o async_ffl.v -r uut -p 'prep; clk2fflogic -neghold; rename uut ffl' async.v
+../../yosys -q -o async_syn.v -p 'read_verilog -icells async.v; hierarchy -top uut' -p 'synth; rename uut syn'
+../../yosys -q -o async_prp.v -p 'read_verilog -icells async.v; hierarchy -top uut' -p 'prep; rename uut prp'
+../../yosys -q -o async_a2s.v -p 'read_verilog -icells async.v; hierarchy -top uut' -p 'prep; async2sync -neghold; rename uut a2s'
+../../yosys -q -o async_ffl.v -p 'read_verilog -icells async.v; hierarchy -top uut' -p 'prep; clk2fflogic -neghold; rename uut ffl'
 iverilog -o async_sim -DTESTBENCH async.v async_???.v
 vvp -N async_sim > async.out
 tail async.out
--- tests/various/async.v
+++ tests/various/async.v
@@ -1,7 +1,7 @@
-`define MAXQ 2
+`define MAXQ 3
 module uut (
 	input clk,
-	input d, r, e,
+	input d, ad, r, e,
 	output [`MAXQ:0] q
 );
 	reg q0;
@@ -28,44 +28,127 @@ module uut (
 			q2 <= d;
 	end
 
-	assign q = {q2, q1, q0};
+
+	wire q3;
+
+// `ifdef TESTBENCH
+// 	always @(posedge clk, negedge r) begin
+// 		if (r)
+// 			q3 <= ad;
+// 		else if (!e)
+// 			q3 <= d;
+// 	end
+// `else
+	\$aldffe #(.WIDTH(1), .CLK_POLARITY(1'b1), .EN_POLARITY(1'b1), .ALOAD_POLARITY(1'b1)) aldff (.CLK(clk), .ALOAD(r), .AD(ad), .EN(e), .D(d), .Q(q3));
+// `endif
+
+	assign q = {q3, q2, q1, q0};
 endmodule
 
-module tst (
-	input clk,
-	input d, r, e,
-	output [`MAXQ:0] q
-);
 
-	reg q0;
-	always @(posedge clk) begin
-		if (r)
-			q0 <= 0;
-		else if (e)
-			q0 <= d;
-	end
 
-	reg q1;
-	always @(posedge clk, posedge r) begin
-		if (r)
-			q1 <= 0;
-		else if (e)
-			q1 <= d;
-	end
-
-	reg q2;
-	always @(posedge clk, negedge r) begin
-		if (!r)
-			q2 <= 0;
-		else if (!e)
-			q2 <= d;
-	end
-
-	assign q = {q2, q1, q0};
-
-endmodule
 
 `ifdef TESTBENCH
+
+// module \$aldff (CLK, ALOAD, AD, D, Q);
+
+//     parameter WIDTH = 0;
+//     parameter CLK_POLARITY = 1'b1;
+//     parameter ALOAD_POLARITY = 1'b1;
+
+//     input CLK, ALOAD;
+//     input [WIDTH-1:0] AD;
+//     input [WIDTH-1:0] D;
+//     output [WIDTH-1:0] Q;
+//     wire pos_clk = CLK == CLK_POLARITY;
+//     wire pos_aload = ALOAD == ALOAD_POLARITY;
+
+
+//    	// a latch to store the async-load value
+//     reg [WIDTH-1:0] latched_rval;
+//     always @*
+//         if (pos_aload) latched_rval = AD;
+
+//     // a regular FF to store the clocked data value
+//     reg [WIDTH-1:0] q_without_reset;
+//     always @(posedge pos_clk)
+//         q_without_reset <= D;
+
+//     // an asynchronous-reset flip-flop to remember last event
+//     reg last_event_was_clock;
+//     always @(posedge pos_clk or posedge pos_aload)
+//         if (pos_aload) last_event_was_clock <= 0;
+//         else      last_event_was_clock <= 1;
+
+//     // output either the latched reset value or clocked data value
+//     assign Q = last_event_was_clock ? q_without_reset : latched_rval;
+
+
+// endmodule
+
+
+module \$aldffe (CLK, ALOAD, AD, EN, D, Q);
+
+    parameter WIDTH = 0;
+    parameter CLK_POLARITY = 1'b1;
+    parameter EN_POLARITY = 1'b1;
+    parameter ALOAD_POLARITY = 1'b1;
+
+    input CLK, ALOAD, EN;
+    input [WIDTH-1:0] AD;
+    input [WIDTH-1:0] D;
+    output [WIDTH-1:0] Q;
+    wire pos_clk = CLK == CLK_POLARITY;
+    wire pos_aload = ALOAD == ALOAD_POLARITY;
+
+
+   	// // a latch to store the async-load value
+    // reg [WIDTH-1:0] latched_rval;
+    // always @*
+    //     if (pos_aload) latched_rval = AD;
+
+    // // a regular FF to store the clocked data value
+    // reg [WIDTH-1:0] q_without_reset;
+    // always @(posedge pos_clk)
+    //     q_without_reset <= D;
+
+    // // an asynchronous-reset flip-flop to remember last event
+    // reg last_event_was_clock;
+    // always @(posedge pos_clk or posedge pos_aload)
+    //     if (pos_aload) last_event_was_clock <= 0;
+    //     else      last_event_was_clock <= 1;
+
+    // // output either the latched reset value or clocked data value
+    // assign Q = last_event_was_clock ? q_without_reset : latched_rval;
+
+	aldffe_bit bit_0 (.CLK(pos_clk), .ALOAD(pos_aload), .AD(AD[0]), .EN(EN), .D(D[0]), .Q(Q[0]));
+
+endmodule
+
+primitive aldffe_bit (Q, CLK, ALOAD, AD, EN, D);
+	input CLK;
+	input ALOAD;
+	input AD;
+	input EN;
+	input D;
+
+	output Q;
+	reg Q;
+
+	table
+		? 1 1 ? ? : ? : 1;
+		? 1 0 ? ? : ? : 0;
+		p ? ? 1 1 : ? : 1;
+		p ? ? 1 0 : ? : 0;
+		* ? ? ? ? : ? : -;
+		? * ? ? ? : ? : -;
+		? ? * ? ? : ? : -;
+		? ? ? * ? : ? : -;
+		? ? ? ? * : ? : -;
+	endtable
+endprimitive
+
+
 module \$ff #(
 	parameter integer WIDTH = 1
 ) (
@@ -84,37 +167,33 @@ module testbench;
 	reg clk;
 	always @(posedge sysclk) clk = (clk === 1'b0);
 
-	reg d, r, e;
+	reg d, ad, r, e;
 
 	wire [`MAXQ:0] q_uut;
-	uut uut (.clk(clk), .d(d), .r(r), .e(e), .q(q_uut));
+	uut uut (.clk(clk), .d(d), .ad(ad), .r(r), .e(e), .q(q_uut));
 
 	wire [`MAXQ:0] q_syn;
-	syn syn (.clk(clk), .d(d), .r(r), .e(e), .q(q_syn));
+	syn syn (.clk(clk), .d(d), .ad(ad), .r(r), .e(e), .q(q_syn));
 
 	wire [`MAXQ:0] q_prp;
-	prp prp (.clk(clk), .d(d), .r(r), .e(e), .q(q_prp));
+	prp prp (.clk(clk), .d(d), .ad(ad), .r(r), .e(e), .q(q_prp));
 
 	wire [`MAXQ:0] q_a2s;
-	a2s a2s (.clk(clk), .d(d), .r(r), .e(e), .q(q_a2s));
+	a2s a2s (.clk(clk), .d(d), .ad(ad), .r(r), .e(e), .q(q_a2s));
 
 	wire [`MAXQ:0] q_ffl;
-	ffl ffl (.clk(clk), .d(d), .r(r), .e(e), .q(q_ffl));
+	ffl ffl (.clk(clk), .d(d), .ad(ad), .r(r), .e(e), .q(q_ffl));
 
-
-	wire [`MAXQ:0] q_tst;
-	tst tst (.clk(clk), .d(d), .r(r), .e(e), .q(q_tst));
 
 	task printq;
 		reg [5*8-1:0] msg;
 		begin
 			msg = "OK";
-			if (q_uut !== q_syn) msg = "SYN";
 			if (q_uut !== q_prp) msg = "PRP";
 			if (q_uut !== q_a2s) msg = "A2S";
 			if (q_uut !== q_ffl) msg = "FFL";
-			if (q_uut !== q_tst) msg = "TST";
-			$display("%6t %b %b %b %b %b %b %s", $time, q_uut, q_syn, q_prp, q_a2s, q_ffl, q_tst, msg);
+			if (q_uut !== q_syn) msg = "SYN";
+			$display("%6t %b %b %b %b %b %s", $time, q_uut, q_syn, q_prp, q_a2s, q_ffl, msg);
 			//if (msg != "OK") $finish;
 		end
 	endtask
@@ -126,19 +205,25 @@ module testbench;
 
 	initial begin
 		@(posedge clk);
+
 		d <= 0;
+		ad <= 0;
 		r <= 0;
 		e <= 0;
 		@(posedge clk);
+
 		e <= 1;
 		@(posedge clk);
+
 		e <= 0;
 		repeat (1000) begin
 			@(posedge clk);
+
 			printq;
 			d <= $random;
+
+			ad <= $random;
 			e <= $random;
-			#2;
 			r <= $random;
 		end
 		$display("PASS");
